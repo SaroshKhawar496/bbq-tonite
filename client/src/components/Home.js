991cd26b5b8a1e36413f4001edf31fbf957269c0
SaroshKhawar496 <sarosh.496@gmail.com>
2019-11-28 22:33:42 -0800

    Added GoogleReview component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import styles from "../sass/Home.module.scss";
+import GoogleReviews from "../containers/GoogleReviews";
+
+
 
 export default class Home extends Component {
   render() {
     return (
-      <section className={styles.mainSection}>
-        <div className={styles.heroText}>
-          <h2 className={styles.mainTitle}>Tandoori Grill</h2>
-          <h3>Flavor Worth Having</h3>
-        </div>
-      </section>
+      <div>
+        <section className={styles.mainSection}>
+          <div className={styles.heroText}>
+            <h2 className={styles.mainTitle}>Tandoori Grill</h2>
+            <h3>Flavor Worth Having</h3>
+          </div>
+        </section>
+        <GoogleReviews />
+      </div>
     );
   }
 }
---
+++ client/src/containers/GoogleReviews.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class GoogleReviews extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is Google Reviews</h1>
+            </div>
+        )
+    }
+}
+
+export default GoogleReviews;
