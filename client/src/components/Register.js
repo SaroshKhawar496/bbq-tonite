5c26655ec8c921c72d7a35e01ae3ca0d88c04285
MImranAsghar <imranasghar96@hotmail.com>
2019-12-07 16:43:44 -0500

    register.js still giving some issues

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,101 +1,81 @@
-/* import React, { useState } from "react";
-import {
-  HelpBlock,
-  FormGroup,
-  FormControl,
-  ControlLabel
-} from "react-bootstrap";
-import { useFormFields } from "../libs/hooksLib";
+import React, { Component, state } from "react";
+import { Button, Form, Alert, Col} from 'react-bootstrap';
+import axios from 'axios';
 
-export default function Signup(props) {
-  const [fields, handleFieldChange] = useFormFields({
-    email: "",
-    password: "",
-    confirmPassword: "",
-    confirmationCode: ""
-  });
-  const [newUser, setNewUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  function validateForm() {
-    return (
-      fields.email.length > 0 &&
-      fields.password.length > 0 &&
-      fields.password === fields.confirmPassword
-    );
+class Signup extends Component{
+  constructor(){
+    super()
+    this.state = {
+      "firstName": "",
+      "lastName": "",
+      "email": "",
+      "password": ""
+    };
+    this.trackChange = this.trackChange.bind(this);
+    this.createUser = this.createUser.bind(this);
   }
 
-  async function handleSubmit(event) {
+  trackChange = (event) => {
     event.preventDefault();
-    setNewUser("test");
+    console.log("event " + event.target.value)
+    const { target: { name, value } } = event;
+    this.setState({[String(name)]: String(value)})
   }
 
-  async function handleConfirmationSubmit(event) {
-    event.preventDefault();
+  createUser = () => {
+    console.log(this.state.firstName)
+    console.log("email" + this.state.email)
+
+    axios({
+      method: 'post',
+      url: `http://localhost:4000/api/users/`,
+      headers: {},
+      data: {
+          "id":"cha",
+          "firstName": "cha",
+          "lastName": "cha",
+          "email": "cha@hotmail.com",
+          "status": "cool",
+          "password": "cha"
+      }
+    })
+      .then(res => {
+        const user = res.data;
+        this.setState({ user });
+      })
   }
 
-  function renderConfirmationForm() {
+  render(){
     return (
-      <form onSubmit={handleConfirmationSubmit}>
-        <FormGroup controlId="confirmationCode" bsSize="large">
-          <ControlLabel>Confirmation Code</ControlLabel>
-          <FormControl
-            autoFocus
-            type="tel"
-            onChange={handleFieldChange}
-            value={fields.confirmationCode}
-          />
-          <HelpBlock>Please check your email for the code.</HelpBlock>
-        </FormGroup>
-      </form>
+    <Form>
+      <Form.Group>
+        <Form.Label>Email address</Form.Label>
+        <Form.Control id="email" type="email" onChange = {this.trackChange.bind(this)} placeholder="Enter email" name="email"/>
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control id="password" type="password" onChange = {this.trackChange.bind(this)} placeholder="Password" name="password"/>
+      </Form.Group>
+      <Form>
+      <Form.Row>
+        <Col>
+          <Form.Control id="firstName" onChange = {this.trackChange.bind(this)} placeholder="First name" name="firstName"/>
+        </Col>
+        <Col>
+          <Form.Control id="lastName" onChange = {this.trackChange.bind(this)} placeholder="Last name" name="lastName"/>
+        </Col>
+      </Form.Row>
+    </Form>
+      <Button variant="primary" type="submit" onClick={this.createUser.bind(this)}>
+        Submit
+      </Button>
+    </Form>
     );
   }
-
-  function renderForm() {
-    return (
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <ControlLabel>Email</ControlLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={fields.email}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            type="password"
-            value={fields.password}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="confirmPassword" bsSize="large">
-          <ControlLabel>Confirm Password</ControlLabel>
-          <FormControl
-            type="password"
-            onChange={handleFieldChange}
-            value={fields.confirmPassword}
-          />
-        </FormGroup>
-        <LoaderButton
-          block
-          type="submit"
-          bsSize="large"
-          isLoading={isLoading}
-          disabled={!validateForm()}
-        >
-          Signup
-        </LoaderButton>
-      </form>
-    );
-  }
-
-  return (
-    <div className="Signup">
-      {newUser === null ? renderForm() : renderConfirmationForm()}
-    </div>
-  );
 }
- */
+
+export default Signup
