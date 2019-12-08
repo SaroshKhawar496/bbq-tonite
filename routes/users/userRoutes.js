f90ee5fa1f8af74d20ce746104781fedf7f73630
SaroshKhawar496 <sarosh.496@gmail.com>
2019-12-08 03:30:27 -0800

    Using yup validation in login route

--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -9,9 +9,9 @@ const tokenService = require("./tokenService");
 
 const { model: UserModel } = require("./userModel");
 const { request: RequestModel } = require("./requestModel");
-const { registerValidation } = require("./validation");
+const { registerValidation, loginValidation } = require("./validation");
 
-// POST /editUsers
+// new user registration route
 userRouter.route("/").post(async (req, res, next) => {
   const { firstName, lastName, email, password } = req.body;
   const { msg, errors } = await registerValidation(req.body);
@@ -38,6 +38,40 @@ userRouter.route("/").post(async (req, res, next) => {
     res.send({ user: savedUser._id });
   } catch (err) {
     return res.status(400).send(err);
+  }
+});
+
+// Login Route
+userRouter.route("/login").post(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //doing validation
+  const { msg, errors } = await loginValidation(req.body);
+  if (msg) {
+    //validation passed
+    try {
+      const user = await UserModel.findOne({ email });
+      if (!user) {
+        return res.status(404).send({ error: "User doesn't exist!" });
+      } else {
+        const match = await user.comparePassword(password);
+        if (match) {
+        //   console.log("passwords match");
+          const token = tokenService.issueToken(user);
+          return res.json({
+            access_token: token
+            //   refresh_token: null,
+            //   refresh: "/api/users/login/refresh"
+          });
+        } else {
+          return res.status(401).send({ error: "Incorrect Password" });
+        }
+      }
+    } catch (e) {
+      next(e);
+    }
+  } else {
+    return res.status(400).send({ error: errors[0] });
   }
 });
 
@@ -77,31 +111,6 @@ userRouter.route("/me").get(async (req, res, next) => {
     res.json(req.user);
   } else {
     res.status(404).send();
-  }
-});
-
-// POST
-userRouter.route("/login").post(async (req, res, next) => {
-  const { email, password } = req.body;
-  try {
-    const user = await UserModel.findOne({ email });
-    if (!user) {
-      return res.status(404).send({ error: "User doesn't exist!" });
-    } else {
-      const match = await user.comparePassword(password);
-      if (match) {
-        const token = tokenService.issueToken(user);
-        res.json({
-          access_token: token
-          //   refresh_token: null,
-          //   refresh: "/api/users/login/refresh"
-        });
-      } else {
-        res.status(401).send();
-      }
-    }
-  } catch (e) {
-    next(e);
   }
 });
 
