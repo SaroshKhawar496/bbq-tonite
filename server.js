cf17cf3e079eb560c5d41becb5a111ab5771fcf7
SaroshKhawar496 <sarosh.496@gmail.com>
2019-12-11 17:23:52 -0800

    Changed server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const dotenv = require("dotenv");
 dotenv.config();
