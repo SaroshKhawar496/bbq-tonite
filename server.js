ade4fef32979a279aeb6d5f91425799f5b0160d6
SaroshKhawar496 <sarosh.496@gmail.com>
2019-12-11 17:27:59 -0800

    Changed server.js

--- server.js
+++ server.js
@@ -35,6 +35,12 @@ const MONGO_CONFIG = {
   useUnifiedTopology: true
 };
 
+// deployment to Heroku
+app.use(express.static(path.join(__dirname, "public")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 mongoose
   .connect(mongoURI, { useMongoClient: true })
   .then(async () => {
@@ -48,11 +54,6 @@ mongoose
     console.error(err);
   });
 
-// deployment to Heroku
-app.use(express.static(path.join(__dirname, "public")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 // // 7. atlasbDB
 // mongoose
 //   .connect(URL, MONGO_CONFIG)
